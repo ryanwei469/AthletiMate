a5ddbe1207becb569b694f1d59b667614f601a9a
abbyspeed <wnabihah09@gmail.com>
2024-09-10 04:35:41 +0800

    modified signup pages

--- lib/signUp.dart
+++ lib/signUp.dart
@@ -1,4 +1,5 @@
 import 'package:athletimate/components/topBar.dart';
+import 'package:athletimate/model/User.dart';
 import 'package:athletimate/signUp2.dart';
 import 'package:flutter/material.dart';
 
@@ -11,6 +12,8 @@ class SignUp extends StatefulWidget{
 
 class _SignUpState extends State<SignUp> {
   TextEditingController _fullNameController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -66,7 +69,7 @@ class _SignUpState extends State<SignUp> {
                     ),
                     TextFormField(
                       validator: (value) {},
-                      controller: _fullNameController,
+                      controller: _emailController,
                       obscureText: false,
                       autocorrect: true,
                       enableSuggestions: false,
@@ -99,7 +102,7 @@ class _SignUpState extends State<SignUp> {
                     ),
                     TextFormField(
                       validator: (value) {},
-                      controller: _fullNameController,
+                      controller: _passwordController,
                       obscureText: true,
                       autocorrect: true,
                       enableSuggestions: false,
@@ -124,7 +127,11 @@ class _SignUpState extends State<SignUp> {
               ElevatedButton(
                 onPressed: (){
                   Navigator.of(context).push(
-                    MaterialPageRoute(builder: (context) => SignUp2())
+                    MaterialPageRoute(builder: (context) => SignUp2(
+                      name: _fullNameController.text,
+                      email: _emailController.text,
+                      pwd: _passwordController.text,
+                    ))
                   );
                 }, 
                 child: Text("Continue")
--- lib/signUp2.dart
+++ lib/signUp2.dart
@@ -1,18 +1,88 @@
 import 'package:athletimate/components/customDropdown.dart';
 import 'package:athletimate/components/customSelection.dart';
 import 'package:athletimate/components/topBar.dart';
+import 'package:athletimate/model/User.dart';
 import 'package:athletimate/signUp3.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:table_calendar/table_calendar.dart';
 
 class SignUp2 extends StatefulWidget{
-  const SignUp2({super.key});
+  const SignUp2({
+    super.key, 
+    required this.name, 
+    required this.email, 
+    required this.pwd
+  });
+  final String name, email, pwd;
 
   @override
   State<SignUp2> createState() => _SignUp2State();
 }
 
 class _SignUp2State extends State<SignUp2> {
+  // final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Map<String, String> _dob = {
+    'month': "Month",
+    'day': "",
+    'year': "",
+  };
+
+  List<String> _selectedSports = [];
+
+  void _onDateOfBirthSelected(Map<String, String> dob) {
+    setState(() {
+      _dob = dob;
+    });
+  }
+
+  void _onSelectionChanged(List<String> selectedSports) {
+    setState(() {
+      _selectedSports = selectedSports;
+    });
+  }
+
+  // A function to create a new user
+  Future<void> _createUser() async {
+    try {
+      // // Create user with email and password
+      // UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      //   email: widget.email,
+      //   password: widget.pwd,
+      // );
+
+      // // Extract user details
+      // User firebaseUser = userCredential.user!;
+      // final user = UserModel(
+      //   userId: firebaseUser.uid,
+      //   name: widget.name,
+      //   email: widget.email,
+      //   pwd: widget.pwd
+      //   dob: '${_dob['month']}/${_dob['day']}/${_dob['year']}', // Format DOB
+      //   selectedSports: _selectedSports,
+      // );
+
+      // // Save user to Firestore
+      // await _firestore.collection('users').doc(user.userId).set({
+      //   'name': user.name,
+      //   'email': user.email,
+      //   'pwd': user.pwd,
+      //   'dob': user.dob,
+      //   'selectedSports': user.selectedSports
+      // });
+
+      // Navigate to the next screen
+      Navigator.of(context).push(
+        MaterialPageRoute(builder: (context) => SignUp3()),
+      );
+    } catch (e) {
+      // Handle errors here
+      print('Error during user creation: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,24 +93,20 @@ class _SignUp2State extends State<SignUp2> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Padding(
+              Padding(
                 padding: EdgeInsets.symmetric(vertical: 8),
                 child: Text("Date of Birth"),
               ),
-              CustomDropdown(),
+              CustomDropdown(onDateOfBirthSelected: _onDateOfBirthSelected),
               const SizedBox(height: 15),
               const Padding(
                 padding: EdgeInsets.symmetric(vertical: 8),
                 child: Text("Sports Tag Selection"),
               ),
-              CustomSelection(),
+              CustomSelection(onSelectionChanged: _onSelectionChanged),
               Center(
                 child: ElevatedButton(
-                  onPressed: (){
-                    Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => SignUp3())
-                    );
-                  }, 
+                  onPressed: _createUser,
                   child: Text("Continue")
                 ),
               )
